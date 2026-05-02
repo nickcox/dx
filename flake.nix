723f5a09251b383ce95e53885e773ca7f396c845
Nick <nickcox1@gmail.com>
2026-05-02 15:09:29 +1000

    Cut version

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
         rec {
           cdex = pkgs.rustPlatform.buildRustPackage {
             pname = "cdex";
-            version = "0.2.0";
+            version = "0.3.0";
             src = ./.;
             cargoLock = {
               lockFile = ./Cargo.lock;
